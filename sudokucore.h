05393982b87e658c8cdfbf9a6b08d6726b6b2cc6
kris9290 <oban200@main.ru>
2026-09-04 11:51:41 +0300

    Add SudokuCore.h with matrix and masks design

---
+++ sudokucore.h
@@ -0,0 +1,34 @@
+#ifndef SUDOKUCORE_H
+#define SUDOKUCORE_H
+
+#include <vector>
+#include <bitset>
+#include <stdexcept>
+
+struct CellPos{
+	int row;
+	int col;
+	int getSectorIndex const{
+		return (row/3)*3 + (col/3);
+	}
+};
+
+class SudokuCore{
+public:
+	SudokuCore();
+	void setCell(int row, int col, int value); //устаговить значение 1-9 или очистить (0)
+	int getCell(int row, int col) const;
+	void clearBoard(); //сбросить поле
+	
+	std::bitset<10> getCellCandidates(int row, int col) const;
+	bool isValidMove(int row, int col, int value) const; 
+private:
+	std::vector<std::vector<int>> m_board;
+
+	std::vector<std::bitset<10>> m_rowMasks;
+	std::vector<std::bitset<10>> m_colMasks;
+	std::vector<std::bitset<10>> m_setcorMasks;
+
+	void updateMasks();
+};
+#endif
